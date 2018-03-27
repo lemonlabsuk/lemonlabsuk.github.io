f522311c4b67f4d2d245d52e87918a88e1a13c5a
Ian Forsey <forsey@gmail.com>
2018-03-27 01:05:34 +0100

    cloudformation update-stack rather than create-stack

--- _cloudformation/deploy_cloudfront.sh
+++ _cloudformation/deploy_cloudfront.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 CERTIFICATE_ARN=$(aws --region us-east-1 acm list-certificates | jq --raw-output '.CertificateSummaryList | map(select(.DomainName == "*.lemonlabs.io"))[0].CertificateArn')
-aws cloudformation create-stack --stack-name lemon-labs-cloudfront --template-body file://cloudfront.json --parameters ParameterKey=AcmCertificateArn,ParameterValue=$CERTIFICATE_ARN
+aws cloudformation update-stack --stack-name lemon-labs-cloudfront --template-body file://cloudfront.json --parameters ParameterKey=AcmCertificateArn,ParameterValue=$CERTIFICATE_ARN
